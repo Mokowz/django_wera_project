6ba322e09768a44d3034b141582fc3c68b2d161d
Mokowz <ronniemokaya30@gmail.com>
2023-09-06 12:55:25 +0300

    Add application model

--- jobs/models.py
+++ jobs/models.py
@@ -20,3 +20,13 @@ class Jobs(models.Model):
     
     def get_absolute_url(self):
         return reverse("job_detail", args=[str(self.pk)])
+    
+
+class Application(models.Model):
+    rate = models.PositiveIntegerField()
+    job = models.ForeignKey(
+        Jobs,
+        on_delete=models.CASCADE
+    )
+    apply = models.TextField()
+
